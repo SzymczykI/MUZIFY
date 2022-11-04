e87956a12277477611a0e8383e58dc257c9499d1
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2022-11-04 11:05:50 +0100

    firebase config and registering user

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import VeeValidatePlugin from "./plugins/validation";
+import "./plugins/firebase";
 
 import "./assets/base.css";
 import "./assets/main.css";
