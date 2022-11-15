35db3e97bf9ffdfde03764fc71b1ade588c1f091
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2022-11-15 22:06:39 +0100

    route transitions and links with hash

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,33 @@
 <template>
-  <!-- Introduction -->
-  <section class="mb-4 py-40 md:py-64 text-white text-center relative">
-    <div
-      class="absolute inset-0 w-full h-full bg-cover bg-no-repeat bg-left sm:bg-center bg-local"
-      style="background-image: url(assets/img/muzify.gif)"
-    ></div>
-  </section>
+  <main>
+    <!-- Introduction -->
+    <section class="mb-4 py-40 md:py-64 text-white text-center relative">
+      <div
+        class="absolute inset-0 w-full h-full bg-cover bg-no-repeat bg-left sm:bg-center bg-local"
+        style="background-image: url(assets/img/muzify.gif)"
+      ></div>
+    </section>
 
-  <!-- Main Content -->
-  <section class="container mx-auto">
-    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-        <span class="card-title">Songs</span>
-        <!-- Icon -->
-        <i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>
+    <!-- Main Content -->
+    <section class="container mx-auto">
+      <div
+        class="bg-white rounded border border-gray-200 relative flex flex-col"
+      >
+        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+          <span class="card-title">Songs</span>
+          <!-- Icon -->
+          <i
+            class="fa fa-headphones-alt float-right text-green-400 text-xl"
+          ></i>
+        </div>
+        <!-- Playlist -->
+        <ol id="playlist">
+          <app-song-item v-for="song in songs" :key="song.docID" :song="song" />
+        </ol>
+        <!-- .. end Playlist -->
       </div>
-      <!-- Playlist -->
-      <ol id="playlist">
-        <app-song-item v-for="song in songs" :key="song.docID" :song="song" />
-      </ol>
-      <!-- .. end Playlist -->
-    </div>
-  </section>
+    </section>
+  </main>
 </template>
 
 <script>
