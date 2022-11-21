2aca99adc2c90d9e8b79d9f4ce0e008779ad5404
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2022-11-21 18:10:20 +0100

    offline ulpoad handler

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -67,6 +67,18 @@ export default {
           return;
         }
 
+        if (!navigator.onLine) {
+          this.uploads.push({
+            task: {},
+            current_progress: 100,
+            name: file.name,
+            variant: "bg-red-400",
+            icon: "fas fa-times",
+            text_class: "text-red-400",
+          });
+          return;
+        }
+
         //good practice to have root reference separated from others
         const songsRef = ref(storage, `songs/${file.name}`);
         const task = uploadBytesResumable(songsRef, file);
